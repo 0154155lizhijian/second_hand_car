b594ca2ec66276fa9f168d6d7db4cdccf144314c
0154155lizhijian <1367688918@qq.com>
2018-11-20 21:23:17 +0800

    进入详情页

---
+++ miniprogram/pages/detail/second_hand_car/index.js
@@ -0,0 +1,34 @@
+wx.cloud.init({ })
+let City = require('../../../utils/allcity.js');
+Page({
+
+  data: {
+    city: City,
+    myCity: '南昌',
+    isCityTrue: false,
+  },
+
+  onLoad: function (options) {
+    
+  },
+  //打开城市选择
+  showCity: function () {
+    this.setData({
+      isCityTrue: true
+    })
+  },
+  //关闭城市选择
+  hideCity: function () {
+    this.setData({
+      isCityTrue: false
+    })
+  },
+
+
+  bindtap(e) {
+    this.setData({
+      myCity: e.detail.name
+    })
+  },
+})
+
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -15,6 +15,9 @@ Page({
     ads: [],
     beforeColor: "#CBD1D9",//指示点颜色
     afterColor: "#22A038",//当前选中的指示点颜色
+    car1:[],
+    car2:[],
+    cars:[],
     classify:[
       {type:'5万以下',
        hasIcon:false,
@@ -62,41 +65,49 @@ Page({
           icon_type :'che',
           color:'#0DC62A',
           fontsize:'40rpx',
+          src:'../detail/second_hand_car/index'
         },{
           category:'严选车',
           icon_type :'mai',
           color:'#6671ED',
           fontsize:'60rpx',
+          src:'../detail/strict_car/index'
         },{
           category:'免费卖车',
           icon_type:'qiandai',
           color:'#13DDB2',
           fontsize:'40rpx',
+          src:'../detail/sale_car/index'
         },{
           category:"快速卖车",
           icon_type:"mai1",
           color:'#FFA441',
           fontsize:'30rpx',
+          src:'../detail/fast_sale_car/index'
         },{
           category:"估价",
           icon_type:"maichegujiapsd01",
           color:'#F75B5B',
           fontsize:'40rpx',
+          src:'../detail/valuation/index'
         },{
           category:"查成交",
           icon_type:"chengjiao",
           color:'#2A92ED',
           fontsize:'40rpx',
+          src:'../detail/deal/index'
         },{
           category:"准新车",
           icon_type:"haopingwuxing",
           color:'#F65655',
           fontsize:'40rpx',
+          src:'../detail/new_car/index'
         },{
           category:"降价急售",
           icon_type:"shandian",
           color:'#FFCF43',
           fontsize:'40rpx',
+          src:'../detail/cheap_car/index'
         }
       ]
       ,[
@@ -104,22 +115,26 @@ Page({
           category:"今日超值",
           icon_type:"fire",
           color:'#FC781B',
-          fontsize:'40rpx'
+          fontsize:'40rpx',
+          src:'../detail/today_great/index'
         },{
           category:"练手车",
           icon_type:"quxuechelian",
           color:'#0CC129',
-          fontsize:'38rpx'
+          fontsize:'38rpx',
+          src:'../detail/test_car/index'
         },{
           category:"热销排行",
           icon_type:"guest-branch",
           color:'#FFA23E',
-          fontsize:'40rpx' 
+          fontsize:'40rpx' ,
+          src:'../detail/top/index'
         },{
           category:"百科",
           icon_type:"baike-tianchong",
           color:'#3194EF',
-          fontsize:'40rpx'
+          fontsize:'40rpx',
+          src:'../detail/knowledge/index'
         }
       ]
     ],
@@ -131,6 +146,29 @@ Page({
       success: res => {
         this.setData({
           ads: res.data
+        })
+      }
+    }),
+
+    db.collection("car").get({
+      success:res => {
+        this.setData({
+          car1:res.data
+        })
+      }
+    }),
+    db.collection("car").skip(20).get({
+      success:res => {
+        this.setData({
+          car2:res.data
+        })
+      }
+    })
+    db.collection("car").skip(40).get({
+      success:res => {
+        var car = [...this.data.car1,...this.data.car2,...res.data]
+        this.setData({
+          cars:car
         })
       }
     })
@@ -154,12 +192,17 @@ Page({
         isCityTrue: false
     })
   },
-
-  
   bindtap(e){
     this.setData({
       myCity:e.detail.name
     })
   },
+  toDetailPage:function(e){
+    let src = e.currentTarget.dataset.src
+    wx.navigateTo({
+      url: src
+    })
+  }
+  
 })
  
